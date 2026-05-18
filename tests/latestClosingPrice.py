002ddc8af260ee9083bfa9bed8ac27948f5b7690
019-Mahansa <lightningplay2009@gmail.com>
2026-05-18 18:11:00 +0700

    first set up

---
+++ tests/latestClosingPrice.py
@@ -0,0 +1,21 @@
+import requests
+
+def get_chart(symbol:str = "BTCUSDT", interval:str = "1d", limit:int = 5):
+    URL_target = "https://data-api.binance.vision/api/v3/klines"
+
+    param = {
+        'symbol': symbol,
+        'interval': interval,
+        'limit': limit
+    }
+    
+    takeINformation = requests.get(URL_target,params=param)
+
+    return takeINformation.json()
+
+dataMentah = get_chart()
+
+for arr in dataMentah:
+    print(float(arr[4]))
+
+    
