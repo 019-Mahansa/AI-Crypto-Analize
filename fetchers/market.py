89f72600fb1fbacd1da44147d86db3195bf0910b
019-Mahansa <lightningplay2009@gmail.com>
2026-05-18 18:10:34 +0700

    fisrt set up

---
+++ fetchers/market.py
@@ -0,0 +1,5 @@
+# import requests
+
+# def get_historical_data(symbol: str = "BTCUSDT", interval: str = "1d", limit: int = 100):
+#     base_url = "https://data-api.binance.vision"
+#     endpoint = "/api/v3/klines"
