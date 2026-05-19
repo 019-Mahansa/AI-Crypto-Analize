00008ea01b9605bedfb2393b92656df9d1a67328
019-Mahansa <lightningplay2009@gmail.com>
2026-05-19 09:55:37 +0700

    move to the main project from tests

--- fetchers/market.py
+++ fetchers/market.py
@@ -1,5 +1,21 @@
-# import requests
+import requests
 
-# def get_historical_data(symbol: str = "BTCUSDT", interval: str = "1d", limit: int = 100):
-#     base_url = "https://data-api.binance.vision"
-#     endpoint = "/api/v3/klines"
+def get_chart(symbol:str = "BTCUSDT", interval:str = "1d", limit:int = 5):
+    URL_target = "https://data-api.binance.vision/api/v3/klines"
+
+    param = {
+        'symbol': symbol,
+        'interval': interval,
+        'limit': limit
+    }
+    
+    takeINformation = requests.get(URL_target,params=param)
+
+    return takeINformation.json()
+
+dataMentah = get_chart()
+
+for arr in dataMentah:
+    print(float(arr[4]))
+
+    
--- tests/latestClosingPrice.py
+++ tests/latestClosingPrice.py
@@ -1,21 +1,21 @@
-import requests
+# import requests
 
-def get_chart(symbol:str = "BTCUSDT", interval:str = "1d", limit:int = 5):
-    URL_target = "https://data-api.binance.vision/api/v3/klines"
+# def get_chart(symbol:str = "BTCUSDT", interval:str = "1d", limit:int = 5):
+#     URL_target = "https://data-api.binance.vision/api/v3/klines"
 
-    param = {
-        'symbol': symbol,
-        'interval': interval,
-        'limit': limit
-    }
+#     param = {
+#         'symbol': symbol,
+#         'interval': interval,
+#         'limit': limit
+#     }
     
-    takeINformation = requests.get(URL_target,params=param)
+#     takeINformation = requests.get(URL_target,params=param)
 
-    return takeINformation.json()
+#     return takeINformation.json()
 
-dataMentah = get_chart()
+# dataMentah = get_chart()
 
-for arr in dataMentah:
-    print(float(arr[4]))
+# for arr in dataMentah:
+#     print(float(arr[4]))
 
     
