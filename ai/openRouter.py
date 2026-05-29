b12ce6f20417e338c313e605cd71f492908baa60
019-Mahansa <lightningplay2009@gmail.com>
2026-05-29 22:43:47 +0700

    connect the dataset to the Ai so the AI can give the resault

--- ai/openRouter.py
+++ ai/openRouter.py
@@ -1,23 +1,45 @@
 from openai import OpenAI
 import os
+import sys
 from dotenv import load_dotenv
-
 load_dotenv()
+
+from rich.console import Console
+from rich.markdown import Markdown
+
+console = Console()
+
+sys.path.append(os.path.abspath("."))
+sys.path.append(os.path.abspath("./fetchers"))
+
+from fetchers.fundamental import get_fundamentals
+from fetchers.market import makeTable
+
+
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
   api_key=os.getenv("OPENROUTER_API"),
 )
 
+def promptUser1():
+  inputText = str(input("What coin do you want to analyze? (Solana, Bitcoin, Ethereum etc) "))
+  return(inputText.lower().strip())
+
+inputText = promptUser1()
+
 response = client.chat.completions.create(
-  model="anthropic/claude-3-haiku",
+  model="google/gemini-2.0-flash-lite-001",
   messages=[
           {
             "role": "user",
-            "content": "give me code of a simple python function that adds two numbers"
+            "content": "Analayze this data and give me a summary of the coin's performance: " + str(get_fundamentals(ids=inputText)) + "along with this market data: " + str(makeTable().tail(5)) + "and give a 1 signal from that data with Target profit and Stop lose"
           }
         ],
   extra_body={"reasoning": {"enabled": True}}
 )
 
-response = response.choices[0].message
-print("Assistant's response:", response.content)
+markdown_response = Markdown(response.choices[0].message.content)
+
+
+console.print("Ai responds:\n")
+console.print(markdown_response)
---
+++ utils.py
@@ -0,0 +1,3 @@
+def promptUser2():
+  inputText = str(input("what is the code of the coin that you want to analyze? (SOL,BTC,ETH, etc) "))
+  return inputText
